ccaa526ebb82f7e6e2a77f4342685316d225b50a
MrAlexGert <alex.gert1812@.gmail.com>
2022-03-18 12:16:26 +0300

    added Footer layout  base

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,27 @@
+import stls from '@/styles/components/layout/Footer.module.sass'
+import { TypeGeneralClassNames } from '@/types/index'
+import { phoneNumber } from '@/config/index'
+import cn from 'classnames'
+import { getClassNames } from '@/helpers/index'
+import { Wrapper } from '@/components/layout'
+
+
+
+
+type TypeFooterProps = TypeGeneralClassNames
+
+const Footer = ({ classNames }: TypeFooterProps) => {
+
+  return (
+    <footer
+      className={
+        cn(stls.container, getClassNames({ classNames })) || undefined
+      }>
+      <Wrapper classNames={[stls.wrapper]}>
+     
+      </Wrapper>
+    </footer>
+  )
+}
+
+export default Footer
